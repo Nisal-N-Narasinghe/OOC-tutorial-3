e16caf0dc3a416b0a9c97f2a6ac4cd648c8bdb7a
Nisal-Narasinghe <narasinghenisal24@gmail.com>
2022-02-28 16:30:28 +0530

    Update ex 1.cpp

---
+++ ex 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+ float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
+ char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
+
+ cout<<setw(5)<<"No"<<setw(15)<<"Name"<<setw(10)<<"Marks"<<endl;
+ 
+ for (int r = 0; r < 5; r++) {
+     cout << setw(5) <<  r+1 
+          << setw(15) << names[r]
+          << setw(10) << setiosflags(ios::fixed) << setprecision(2) << marks[r] << endl;
+ }
+}
